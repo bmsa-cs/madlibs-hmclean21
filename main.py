52f214566b9851ab4132971a7b111322cbe8de3f
Hunter McLean <hmclean21@biomedscienceacademy.org>
2020-11-17 14:03:46 +0000

    first draft

--- main.py
+++ main.py
@@ -5,3 +5,19 @@ Period/Core:
 
 
 """
+name = input ("enter a name")
+place = input ("enter a place")
+food = input ("enter a food")
+adjective = input ("enter a adjective")
+verb = input ("enter a verb")
+number = input("enter a number")
+color = input("enter a color")
+smell = input("enter a smell")
+
+
+print("Good old",name,"has just turned",number,"years old")
+print("and his favriote thing to eat is",food,"because it mades him")
+print("very",adjective,". He also likes it because it is the")
+print("color",color,", its also smells like",smell,". His favriote")
+print("place to eat is",place,"because he likes to watcth the people")
+print("while they",verb,".")
